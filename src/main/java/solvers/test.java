85dfaf6c6617ab7e1a302a46982931e70f8202e2
Xun <xun.li@student.maastrichtuniversity.nl>
2024-04-17 00:25:19 +0200

    up

--- src/main/java/solvers/test.java
+++ src/main/java/solvers/test.java
@@ -9,9 +9,9 @@ public class test {
 
         golfgame g=new golfgame();
         // bot bot=new bot();
-        double[] x={17.0,46.0,2.6999999999999993,-6.842727272727275};
+        double[] x={17.0,46.0,4.5,-6};
         double[] a={0.06,0.10};             // a[0] is kenitic friction. a[1] is Static friction
-        double dt=0.01;
+        double dt=0.05;
         double[] hole={33.5,9.97};
         double r=0.15;
 
